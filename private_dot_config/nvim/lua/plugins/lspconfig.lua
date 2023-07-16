43a2e8854f0d80605e426ff86df1d3401e95b92c
Giuseppe Rota <403432+grota@users.noreply.github.com>
2023-07-16 18:06:51 +0200

    savepoint

---
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,39 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		init = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+			keys[#keys + 1] = { "<leader>cl", false }
+			keys[#keys + 1] = { "<leader>cL", "<cmd>LspInfo<cr>", desc = "Lsp Info" }
+		end,
+		opts = function(_, opts)
+			opts.autoformat = false
+			-- return true if you don't want this server to be setup with lspconfig
+			opts.setup.clangd = function(_, o)
+				o.capabilities.offsetEncoding = { "utf-16" }
+				return false
+			end
+			opts.setup.yamlls = function(_, o)
+				o.settings = {
+					yaml = {
+						keyOrdering = false,
+					},
+				}
+				return false
+			end
+			-- opts.setup["*"] = function(server, o)
+			-- 	vim.print(server)
+			-- 	vim.print(o)
+			-- 	return false
+			-- end
+		end,
+		dependencies = {
+			"SmiteshP/nvim-navbuddy",
+			dependencies = {
+				"SmiteshP/nvim-navic",
+				"MunifTanjim/nui.nvim",
+			},
+			opts = { lsp = { auto_attach = true } },
+		},
+	},
+}
