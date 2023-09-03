3016921e1b91507c784bcfc84946ca209cb8c980
Giuseppe Rota <403432+grota@users.noreply.github.com>
2023-09-03 12:34:02 +0200

    update

--- private_dot_config/nvim/lua/plugins/mine.lua
+++ private_dot_config/nvim/lua/plugins/mine.lua
@@ -10,6 +10,57 @@ return {
     event = 'VeryLazy',
   },
   {
+    "jackMort/ChatGPT.nvim",
+    keys ={
+      { '<leader>cc', "<cmd>ChatGPT<CR>", mode = 'n', desc = 'ChatGPT' },
+      { '<leader>ce', "<cmd>ChatGPTEditWithInstruction<CR>", mode = {'n', 'v'}, desc = 'ChatGPT Edit' },
+      { '<leader>cR', ":ChatGPTRun ", mode = {'n', 'v'}, desc = 'ChatGPT run' },
+    },
+    config = function()
+      require("chatgpt").setup({
+        chat = {
+          keymaps = {
+            close = { "<Esc>" },
+            -- yank_last = "<C-y>",
+            -- yank_last_code = "<C-k>",
+            -- scroll_up = "<C-u>",
+            -- scroll_down = "<C-d>",
+            -- new_session = "<C-n>",
+            -- cycle_windows = "<Tab>",
+            -- cycle_modes = "<C-f>",
+            -- select_session = "<Space>",
+            -- rename_session = "r",
+            -- delete_session = "d",
+            -- draft_message = "<C-d>",
+            -- toggle_settings = "<C-o>",
+            -- toggle_message_role = "<C-r>",
+            -- toggle_system_role_open = "<C-s>",
+            -- stop_generating = "<C-x>",
+          },
+        },
+        edit_with_instructions = {
+          keymaps = {
+            close = "<Esc>",
+            -- accept = "<C-y>",
+            -- toggle_diff = "<C-d>",
+            -- toggle_settings = "<C-o>",
+            -- cycle_windows = "<Tab>",
+            use_output_as_input = "<C-t>",
+          },
+        },
+        popup_layout = {
+          default = "right",
+        }
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    event = "VeryLazy",
+  },
+  {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, 2, {
