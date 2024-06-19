effd220c5ebb8570ea4ee9581c49e0cc0320addf
Giuseppe Rota <403432+grota@users.noreply.github.com>
2024-06-19 17:03:17 +0200

    up

--- private_dot_config/nvim/lua/plugins/mine.lua
+++ private_dot_config/nvim/lua/plugins/mine.lua
@@ -110,66 +110,69 @@ return {
 		},
 	},
 
-	{
-		"nanozuki/tabby.nvim",
-		config = function()
-			require("tabby").setup()
-		end,
-		init = function()
-			local presets = require("tabby.presets")
-			local api = require("tabby.module.api")
-			local tab_name = require("tabby.feature.tab_name")
-			local hl_tabline_sel = "TabLineSel"
-			local hl_tabline = "TabLine"
-			local function tab_label(tabid, active)
-				local icon = active and "" or "" -- "󰝤" or "" -- '' or ''
-				-- local number = api.get_tab_number(tabid)
-				local buf_name = require("tabby.feature.buf_name")
-				local opts = {
-					name_fallback = function(tabid2)
-						local wins = api.get_tab_wins(tabid2)
-						local cur_win = api.get_tab_current_win(tabid2)
-						local name = ""
-						if api.is_float_win(cur_win) then
-							name = "[Floating]"
-						else
-							name = buf_name.get(cur_win)
-						end
-						if active and vim.bo[0].modified then
-							name = name .. "+"
-						end
-						if #wins > 1 and not active then
-							name = string.format("%s[%d]", name, #wins)
-						end
-						return name
-					end,
-				}
-				local name = tab_name.get(tabid, opts)
-				return string.format(" %s %s ", icon, name)
-				-- return string.format(' %s %d: %s ', icon, number, name)
-			end
-			presets.active_wins_at_tail.head = nil
-			presets.active_wins_at_tail.tail = nil
-			presets.active_wins_at_tail.win.label = function()
-				return ""
-			end
-			presets.active_wins_at_tail.top_win.label = function()
-				return ""
-			end
-			presets.active_wins_at_tail.active_tab.label = function(tabid)
-				return {
-					tab_label(tabid, true),
-					hl = hl_tabline_sel,
-				}
-			end
-			presets.active_wins_at_tail.inactive_tab.label = function(tabid)
-				return {
-					tab_label(tabid),
-					hl = hl_tabline,
-				}
-			end
-		end,
-	},
+  {
+    "nanozuki/tabby.nvim",
+    config = function()
+      require("tabby").setup({
+        preset = 'tab_only'
+      })
+    end,
+    -- might be that all of this is not useful anymore since preset='tab_only' is used.
+    init = function()
+      local presets = require("tabby.presets")
+      local api = require("tabby.module.api")
+      local tab_name = require("tabby.feature.tab_name")
+      local hl_tabline_sel = "TabLineSel"
+      local hl_tabline = "TabLine"
+      local function tab_label(tabid, active)
+        local icon = active and "" or "" -- "󰝤" or "" -- '' or ''
+        -- local number = api.get_tab_number(tabid)
+        local buf_name = require("tabby.feature.buf_name")
+        local opts = {
+          name_fallback = function(tabid2)
+            local wins = api.get_tab_wins(tabid2)
+            local cur_win = api.get_tab_current_win(tabid2)
+            local name = ""
+            if api.is_float_win(cur_win) then
+              name = "[Floating]"
+            else
+              name = buf_name.get(cur_win)
+            end
+            if active and vim.bo[0].modified then
+              name = name .. "+"
+            end
+            if #wins > 1 and not active then
+              name = string.format("%s[%d]", name, #wins)
+            end
+            return name
+          end,
+        }
+        local name = tab_name.get(tabid, opts)
+        return string.format(" %s %s ", icon, name)
+        -- return string.format(' %s %d: %s ', icon, number, name)
+      end
+      presets.active_wins_at_tail.head = nil
+      presets.active_wins_at_tail.tail = nil
+      presets.active_wins_at_tail.win.label = function()
+        return ""
+      end
+      presets.active_wins_at_tail.top_win.label = function()
+        return ""
+      end
+      presets.active_wins_at_tail.active_tab.label = function(tabid)
+        return {
+          tab_label(tabid, true),
+          hl = hl_tabline_sel,
+        }
+      end
+      presets.active_wins_at_tail.inactive_tab.label = function(tabid)
+        return {
+          tab_label(tabid),
+          hl = hl_tabline,
+        }
+      end
+    end,
+  },
 
 	{
 		"mbbill/undotree",
