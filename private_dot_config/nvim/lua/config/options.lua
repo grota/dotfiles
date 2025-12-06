c08a4f4471d509c2e8efceec6ab9b3f674e49db5
Giuseppe Rota <403432+grota@users.noreply.github.com>
2025-12-06 23:27:26 +0100

    experimental nvim osc52 and tmux

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -19,3 +19,24 @@ vim.g.autoformat = false
 vim.g.snacks_animate = false
 vim.o.wrap = true
 vim.g.lazyvim_python_lsp = 'basedpyright'
+
+-- LazyVim at the time of this writing https://github.com/LazyVim/LazyVim/blob/c64a61734fc9d45470a72603395c02137802bc6f/lua/lazyvim/config/options.lua#L57
+-- does not set vim.o.clipboard when in SSH (at the end of the day I think it's mainly for performance reasons)
+-- but, for comfort, I want it even in ssh, and since we do that we also need to set g:clipboard below,
+-- see nvim's runtime/autoload/provider/clipboard.vim and runtime/lua/vim/provider/health.lua
+vim.o.clipboard = 'unnamedplus'
+-- BufEnter event because vim.g.termfeatures is set very late and asyncronously, see runtime/plugin/osc52.lua.
+-- Even using TermResponse did not work.
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    if vim.g.clipboard then
+      return
+    end
+    -- Calling vim.fn['provider#clipboard#Executable']() here wreaks havok with the rest of nvim.
+    if vim.g.termfeatures and vim.g.termfeatures.osc52 then
+      vim.g.clipboard = 'osc52'
+    elseif os.getenv('TMUX') then
+      vim.g.clipboard = 'tmux'
+    end
+  end,
+})
